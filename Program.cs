d903d2146d0f2d011f9229860929f3a1ea278ac1
Gaurav Mohabe <gmohabe12@gmail.com>
2023-02-11 23:26:28 +0530

    Added: Linked List Problems

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Data_structure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List program");
+        }
+    }
+}
