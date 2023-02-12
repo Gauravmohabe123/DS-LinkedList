983fc75c22b357ba9cacefa929817618ec3a41e2
Gaurav Mohabe <gmohabe12@gmail.com>
2023-02-12 22:28:10 +0530

    Addes: simple linked list problem

---
+++ Linkedlist.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace linked_list
+{
+    internal class Linkedlist
+    {
+        internal class UnsortedList<T>
+        {
+            private Node head;
+            public class Node
+            {
+                public T data;
+                public Node Next;
+                public Node(T data)
+                {
+                    this.data = data;
+                }
+            }
+
+            public bool Add(T data)
+            {
+                Node node = new Node(data);
+                if (head == null)
+                {
+                    head = node;
+                    return true;
+                }
+                node.Next = head;
+                head = node;
+                return true;
+            }
+            public override string ToString()
+            {
+                if (head == null)
+                {
+                    return null;
+                }
+                Node t = head;
+                string a = "";
+                while (t != null)
+                {
+                    a = a + t.data + " ";
+                    t = t.Next;
+                }
+                return a;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Data_structure
 {
     internal class Program
     {
-        static void Main(string[] args)
+       public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked List program");
+
+            List<int> list = new List<int>();
+            list.Add(50);
+            list.Add(30);
+            list.Add(70);
+            Console.WriteLine(list.ToString());
         }
     }
 }
